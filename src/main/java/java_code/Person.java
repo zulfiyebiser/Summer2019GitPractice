89ebc0d7ad32ad7aa2684432441a7893e71f7999
zulfiyebiser <zlfdnbsr@gmail.com>
2019-12-05 14:29:46 -0600

    new project

---
+++ src/main/java/java_code/Person.java
@@ -0,0 +1,8 @@
+package java_code;
+
+public class Person {
+    private int age;
+    public Person(int age){
+        this.age= age;
+    }
+}
---
+++ src/main/java/java_code/Student.java
@@ -0,0 +1,9 @@
+package java_code;
+
+import sun.jvm.hotspot.debugger.Page;
+
+public class Student extends Person {
+    public Student(int age){
+        super(age);
+    }
+}
